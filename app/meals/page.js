99d34518d193581cf4368c145d5d465704e221cc
agcl82 <agcl82@gmail.com>
2025-05-03 20:27:15 -0400

    Adding Mail Page, Browse Meals, Meals Detail, Backend Config, Error page, Not found Page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+
+import styles from './page.module.css';
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from '@/lib/meals.js'
+
+const Meals = async () => {
+    const meals = await getMeals();
+    return <MealsGrid  meals={meals}/>;
+};
 
 const MealsPage = () => {
+
     return (
-        <main>
-            <h1>Meals </h1>
+    <>
+        <header className={styles.header}>
+            <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
+            <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+            <p className={styles.cta}>
+                <Link href="/meals/share">Share Your Favorite Recipe</Link>
+            </p>
+        </header>
+        <main className={styles.main}>
+            <Suspense fallback={<p className={styles.loading}>Loading Meals...</p>}>
+                <Meals />
+            </Suspense>
         </main>
-    );
-};
+    </>
+)};
 
 export default MealsPage;
